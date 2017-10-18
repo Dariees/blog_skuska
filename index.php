b37efd9a2df1aaded2bb790f6cd851d15726c621
Your Name <dsztachov@gmail.com>
2017-10-18 21:47:02 +0200

    SQL command

--- index.php
+++ index.php
@@ -12,6 +12,13 @@ $connection = [
 // v případě chyby vyhodí Dibi\Exception
 $database = new Dibi\Connection($connection);
 //$res = $database->query('SELECT * FROM post');
+$sql = <<<SQL
+SELECT *
+FROM post
+ORDER BY created_at 
+LIMIT 3
+SQL;
+
 $post = $database->query('SELECT * FROM post ORDER BY id');
 
 require '@main.html.php';
